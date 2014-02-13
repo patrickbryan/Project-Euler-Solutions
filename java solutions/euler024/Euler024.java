49c57abef5190b4118ca7f640d06cee4d5b3a657
zekling <patrickmbryan@gmail.com>
2014-02-13 14:22:07 -0500

    euler 24 solution

---
+++ java solutions/euler024/Euler024.java
@@ -0,0 +1,36 @@
+package euler.solutions;
+
+/**
+ *
+ * @author Patrick Bryan
+ */
+public class Euler024 {
+
+    public String solution() {
+        String result = "", numbers = "0123456789";
+        int cur = 0, temp, count;
+        final int max = 1000000;
+
+        for (int i = 10; i >= 1; i--) {
+            count = 0;
+            temp = factorial(i) / i;
+            while (cur + temp < max) {
+                cur += temp;
+                count++;
+            }
+            result += String.valueOf(numbers.charAt(count));
+            numbers = count == 0 ? numbers.substring(1) : 
+                    numbers.substring(0, count) + numbers.substring(count + 1);
+        }
+
+        return result;
+    }
+
+    private int factorial(int n) {
+        int val = 1;
+        for (int i = 2; i <= n; i++) {
+            val *= i;
+        }
+        return val;
+    }
+}

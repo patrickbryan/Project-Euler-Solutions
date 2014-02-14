104661782800b6b11c41b5460920f0cefe47870a
zekling <patrickmbryan@gmail.com>
2014-02-13 21:26:07 -0500

    euler 28 solution

---
+++ java solutions/euler028/Euler028.java
@@ -0,0 +1,23 @@
+package euler.solutions;
+
+/**
+ *
+ * @author Patrick Bryan
+ */
+public class Euler028 {
+    public String solution() {
+        int sum = 1;
+        int max = 1001;
+        int k = 2;
+        
+        for (int i = 3; i <= max; i+=2) {
+            sum += (i * i);
+            sum += (i * i) - ((i - k) * 2);
+            sum += (i * i) - ((i - k) * 4);
+            sum += (i * i) - ((i - k) * 6);
+            k++;
+        }
+        
+        return String.valueOf(sum);
+    }
+}

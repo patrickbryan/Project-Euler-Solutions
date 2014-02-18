02faa9cf5f2c18f2cce08f9f8b5367373f8c0bdf
zekling <patrickmbryan@gmail.com>
2014-02-18 12:25:33 -0500

    Euler 18 and 67 solutions

---
+++ java solutions/euler018/Euler018.java
@@ -0,0 +1,38 @@
+package euler.solutions;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Patrick Bryan
+ */
+public class Euler018 {
+
+    final int TREE_DEPTH = 15;
+
+    public String solution() throws FileNotFoundException {
+        File treeFile = new File("tree18.txt");
+        Scanner sc = new Scanner(treeFile);
+        int[][] bTree = new int[TREE_DEPTH][];
+        int curDepth = 0;
+
+        while (sc.hasNextInt()) {
+            bTree[curDepth] = new int[curDepth + 1];
+            for (int i = 0; i < curDepth + 1; i++) {
+                bTree[curDepth][i] = sc.nextInt();
+            }
+            curDepth++;
+        }
+
+        for (int d = --curDepth; d > 0; d--) {
+            for (int i = 0; i < d; i++) {
+                bTree[d - 1][i] += bTree[d][i] > bTree[d][i + 1]
+                        ? bTree[d][i] : bTree[d][i + 1];
+            }
+        }
+
+        return String.valueOf(bTree[0][0]);
+    }
+}

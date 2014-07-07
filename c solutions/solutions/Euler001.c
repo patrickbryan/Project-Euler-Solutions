67bdf801851f27cfe9c36d5d07f7ae6dcccbaf3e
zekling <patrickmbryan@gmail.com>
2014-07-07 16:19:15 -0400

    euler 1 solution C

---
+++ c solutions/solutions/Euler001.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void euler001() {
+    int sum = 0, i;
+    
+    for (i = 1; i < 1000; i++) {
+        if (i % 3 == 0 || i % 5 == 0)
+            sum += i;
+    }
+    
+    printf("%d\n", sum);
+}
---
+++ c solutions/solutions/main.c
@@ -0,0 +1,14 @@
+/* 
+ * File:   main.c
+ * Author: Tricio
+ *
+ * Created on July 7, 2014, 4:09 PM
+ */
+
+int main(void) {
+
+    euler001();
+    
+    return (0);
+}
+
